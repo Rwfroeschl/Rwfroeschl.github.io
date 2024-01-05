46df4c5473c17206985381bdb6cb94a9ad5f6343
Robert Froeschl <rwfroeschl@gmail.com>
2024-01-05 17:25:23 -0600

    added on a footer to the page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from "@material-ui/icons/Email";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import "../styles/Footer.css";
 
 function Footer() {
-  return <div> Footer </div>;
+  return ( 
+  <div className= "footer">
+    <div className="socialMedia"> 
+      <EmailIcon />
+      <InstagramIcon/>
+      <LinkedIcon />
+      <FacebookIcon />
+    </div>
+    <p> &copy; 2024 rwfroeschl.github.io</p>
+     </div>
+    );
 }
 
 export default Footer;
