4aa6526c92ea36a3bd73e11d12eca77f0d464851
Robert Froeschl <rwfroeschl@gmail.com>
2024-01-08 18:11:09 -0600

    built experience page and added CMS

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,70 @@
 import React from 'react';
-
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from '@material-ui/icons/School';
+import WorkIcon from '@material-ui/icons/Work';
 function Experience() {
   return (
     <div className="Experience">
-      <h1>Experience</h1>
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement 
+          className="vertical-timeline-element--education"
+          date="2014 - 2019"
+          iconStyle={{background: "#3e497a", color: "#fff"}}
+          icon={<SchoolIcon />}
+          >
+            <h3 className="vertical-timeline-element-title"> 
+              Rockhurst High School, Kansas City, Missouri
+            </h3>
+            <p> High School Diploma </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement 
+          className="vertical-timeline-element--work"
+          date="July 2023 - Present"
+          iconStyle={{background: "#e9d35b", color: "#fff"}}
+          icon={<WorkIcon />}
+          >
+            <h3 className="vertical-timeline-element-title"> 
+              Front-End Student Web Developer - KU Information Technology
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Lawrence, Kansas
+            </h4>
+            <p> Develope and update UI for all webpages associated with the University of Kansas </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement 
+          className="vertical-timeline-element--work"
+          date="June 2023 - May 2023"
+          iconStyle={{background: "#e9d35b", color: "#fff"}}
+          icon={<WorkIcon />}
+          >
+            <h3 className="vertical-timeline-element-title"> 
+              Software Engineer Intern - C2FO 
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Leawood, Kansas
+            </h4>
+            <p> 
+              Collaborated with a 12-person team responsible for the backend extract, transform
+              and load (ETL) process for integration with buyers and funding partners.
+            </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement 
+          className="vertical-timeline-element--education"
+          date="2019 - 2024"
+          iconStyle={{background: "#3e497a", color: "#fff"}}
+          icon={<SchoolIcon />}
+          >
+            <h3 className="vertical-timeline-element-title"> 
+              University of Kansas, Lawrence, Kansas
+            </h3>
+            <p> 
+                Bachelors in Computer Science 
+                <br/> 
+                Cyber Security Certificate
+            </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from "@material-ui/icons/Email";
+import "../styles/Home.css";
 function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <div className="about"> 
+        <h2> Hi, My name is Robert</h2>
+        <div className="prompt"> 
+          <p> A software developer with a passion for learning and creating</p>
+          <LinkedInIcon/>
+          <GitHubIcon/>
+          <EmailIcon/> 
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1> 
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span> React, Flutter, HTML, CSS, Flutter, BootStrap, Yarn, MaterialUI </span>
+          </li>
+          <li className="item">
+            <h2> Back-End</h2>
+            <span> WHM, AWS S3, Firebase </span>
+          </li>
+          <li className="item">
+            <h2> Languages </h2>
+            <span> JavaScript, Python, C++, C, Go</span>
+          </li>
+        </ol>
+        </div>
     </div>
   );
 }
