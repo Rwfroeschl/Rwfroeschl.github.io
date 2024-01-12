e4dd492ea4e9d3fc0a404c12b5c50ae74b5bd5d1
Robert Froeschl <rwfroeschl@gmail.com>
2024-01-11 18:38:08 -0600

    Added project page and css

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,15 @@
+import WeatherApp from "../assets/weatherApp-home.png";
+import IOSapp from "../assets/IOSapp-home.jpeg";
+
+export const ProjectList = [
+    {
+        name: "Weather Site",
+        image: WeatherApp,
+        skill: "JavaScript, HTML, CSS, APIs",
+    },
+    {
+        name: "IOS app",
+        image: IOSapp,
+        skill: "Swift, Xcode, UIKit",
+    },
+]
